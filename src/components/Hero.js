0e6599979535583da56bfe2983241b22d1ee9975
Yonaswg <110293537+Yonaswg@users.noreply.github.com>
2024-01-25 14:27:05 +0300

    third commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,21 +6,21 @@ function Hero() {
   // Add your carousel items
   const carouselItems = [
     {
-      imageSrc: "/images/soft.png",
+      imageSrc: "/images/softt.png",
       alt: "Slide 1",
-      text: "Software | Infrastructure | Training | It Outsourcing",
+      text: "Software Infrastructure",
       portfolioLink: "/portfolio",
       contactLink: "/contact",
     },
     {
-      imageSrc: "/images/soft.png",
+      imageSrc: "/images/softt.png",
       alt: "Slide 2",
       text: "Software Development",
       portfolioLink: "/portfolio",
       contactLink: "/contact",
     },
     {
-      imageSrc: "/images/soft.png",
+      imageSrc: "/images/softt.png",
       alt: "Slide 3",
       text: "Software Development",
       portfolioLink: "/portfolio",
@@ -55,20 +55,36 @@ function Hero() {
 
   return (
     <div>
-      <Carousel {...carouselConfig}>
-        {carouselItems.map((item, index) => (
-          <div key={index} style={{ position: "relative", overflow: "hidden" }} >
-            <div style={{ fontSize:35, position: "absolute", top: 0, left: 0, right: 0, bottom: 0, background: "rgba(0, 0, 0, 0.05)", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "flex-start", padding: "20px", color: "black" }}>
-             <div style={{position: "absolute",  top:250, left:70}}><p>{item.text}</p>
-              <a href={item.portfolioLink} style={{ color: "#333", textDecoration: "none", marginTop: "10px", marginRight:"20px" }}>Portfolio</a>
-              <a href={item.contactLink} style={{ color: "#333", textDecoration: "none", marginTop: "10px" }}>Contact</a>
-              </div>
-            </div>
-            <img src={item.imageSrc} alt={item.alt} className="w-full h-auto" />
+    <Carousel {...carouselConfig}>
+      {carouselItems.map((item, index) => (
+        <div key={index} style={{ position: "relative", overflow: "hidden", height: "600px", marginBottom: "20px" }}>
+          <img src={item.imageSrc} alt={item.alt}  className="w-full h-full object-center object-contain" />
+          <div style={{ fontSize: 35, fontWeight: "bold", position: "absolute", top: 0, left: 0, right: 0, bottom: 10, background: "rgba(0, 0, 0, 0.05)", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "flex-start", padding: "20px", color: "darkblue" }}>
+          <div className="relative left-10  sm:left-20 top-40">
+  <p>{item.text}</p>
+  <a 
+    href={item.portfolioLink}
+    className="button-style hover:bg-blue-700 hover:border-blue-700 text-lg sm:text-lg hover:text-white"
+    style={{ textDecoration: "none", marginTop: "20px", marginRight: "20px", border: "2px solid blue", borderRadius: "10px", padding: "8px" }}
+  >
+    Portfolio
+  </a>
+  <a
+    href={item.contactLink}
+    className="button-style hover:bg-red-700 hover:border-red-700 text-lg hover:text-white"
+    style={{ textDecoration: "none", marginTop: "10px", border: "2px solid red", borderRadius: "10px", padding: "8px" }}
+  >
+    Contact
+  </a>
+</div>
+
+
+
           </div>
-        ))}
-      </Carousel>
-    </div>
+        </div>
+      ))}
+    </Carousel>
+  </div>
   );
 }
 
