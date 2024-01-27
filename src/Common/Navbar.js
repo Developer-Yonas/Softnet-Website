eda33e6eae5b29572034212b1978f25a3cd64a93
Yonaswg <110293537+Yonaswg@users.noreply.github.com>
2024-01-27 11:27:15 +0300

    seventh commit

--- src/Common/Navbar.js
+++ src/Common/Navbar.js
@@ -64,7 +64,7 @@ const Navbar = () => {
         <button
           data-collapse-toggle="navbar-dropdown"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-grey rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-11 h-11 justify-center text-grey rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-dropdown"
           aria-expanded={isMenuOpen ? 'true' : 'false'}
           onClick={() => toggleMenu()}
