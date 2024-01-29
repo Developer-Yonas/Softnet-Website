a6d2a5da87ee2b9feae37f9bcee728e5518d30fe
Yonaswg <110293537+Yonaswg@users.noreply.github.com>
2024-01-29 16:53:39 +0300

    tenth commit

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 function Contact() {
@@ -10,16 +9,3 @@ function Contact() {
 }
 
 export default Contact;
-=======
-
-
-function Contact() {
-  return (
-    <>
-     <h1 className="text-4xl text-violet-800 font-semibold mt-5 ml-5 mb-6">Contact Us</h1>
-    </>
-  );
-}
-
-export default Contact;
->>>>>>> aab8db78e95251b1855a462364d99db297044477
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Hero from "../components/Hero";
 import React from "react";
 import Service from "./Service";
@@ -16,22 +15,3 @@ function Home() {
 }
 
 export default Home;
-=======
-import Hero from "../components/Hero";
-import React from "react";
-import Service from "./Service";
-import Client from "../components/Client";
-import Portfolio from "./Portfolio";
-function Home() {
-  return (
-    <>
-     <Hero/><br/>
-     <Service/>
-     <Portfolio/>
-     <Client/>
-    </>
-  );
-}
-
-export default Home;
->>>>>>> aab8db78e95251b1855a462364d99db297044477
