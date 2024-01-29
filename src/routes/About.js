5361a6f21dfce27872bb3dc6b32549eeb903dfb0
Yonaswg <110293537+Yonaswg@users.noreply.github.com>
2024-01-29 15:49:58 +0300

    tenth commit

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function About() {
   return (
-    <div className="bg-gray-50 mb-10 min-h-screen flex flex-col items-center justify-center">
-      <div className="max-w-7xl mx-auto p-6 bg-white rounded-lg shadow-md">
+    <div className="bg-gray-50 overflow-x-hidden mb-10 min-h-full flex flex-col items-center justify-center">
+      <div className="max-w-7xl mx-0 ml-5 p-6 bg-white rounded-lg shadow-md">
         <h1 className="text-4xl text-violet-800 font-semibold mb-6">About Us</h1>
          {/* Image Section */}
          <div className="mb-6">
@@ -11,19 +11,19 @@ function About() {
         </div>
         <h2 className="text-2xl text-gray-500 font-bold mb-6">Softnet Solutions </h2>
         <p className="text-gray-700 leading-relaxed mb-6">
-          Softnet is a technology firm specializing in comprehensive technology training, tailored software development, and end-to-end technology infrastructure solutions.
+          Softnet is a technology firm specializing in comprehensive technology training, tailored<br/> software development, and end-to-end technology infrastructure solutions.
         </p>
         
        
 
         <h2 className="text-2xl text-gray-500 font-bold mb-6">Mission</h2>
         <p className="text-gray-700 leading-relaxed mb-6">
-          Our mission is to provide creative and innovative technology solutions that effectively address the challenges of our community, while also ensuring that these solutions are scalable and globally applicable.
+          Our mission is to provide creative and innovative technology solutions that effectively address the challenges of our community, while also ensuring <br/> that these solutions are scalable and globally applicable.
         </p>
 
         <h2 className="text-2xl text-gray-500 font-bold mb-6">Vision</h2>
         <p className="text-gray-700 leading-relaxed mb-6">
-          Our vision is to be an exceptional and sustainable Ethiopian company that delivers innovative technology solutions that have a significant positive impact on people's lives.
+          Our vision is to be an exceptional and sustainable Ethiopian company that delivers innovative technology solutions that have a significant <br/> positive impact on people's lives.
         </p>
 
         {/* Add more content as needed */}
