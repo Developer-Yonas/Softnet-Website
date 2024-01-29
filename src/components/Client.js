223b0c3b53d1b852bfdf612943b4f78937249d19
Yonaswg <110293537+Yonaswg@users.noreply.github.com>
2024-01-29 09:57:14 +0300

    seventh commit

--- src/components/Client.js
+++ src/components/Client.js
@@ -3,7 +3,7 @@ import ClientSlider from "./ClientSlider"; // Adjust the path based on your proj
 
 const Client = () => {
   return (
-    <div className="container mx-auto my-9">
+    <div className="container mx-0 ml-9 my-9">
       <h2 className="text-4xl text-violet-900 font-semibold mb-4">Our Clients</h2>
       <ClientSlider />
     </div>
--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -15,7 +15,7 @@ const Portfolio = () => {
 
   return (
     <div>
-      <h2 className="text-4xl text-violet-900 font-semibold mb-5 mt-10 ml-10"> Portfolio</h2>
+      <h2 className="text-4xl text-violet-900 font-semibold mb-5 mt-10 ml-5"> Portfolio</h2>
       <section className="text-gray-400 bg-gray-900 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
