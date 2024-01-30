e288dccad5560d9e63e19ed2789f36c5edbe3f37
Yonaswg <110293537+Yonaswg@users.noreply.github.com>
2024-01-30 15:14:56 +0300

    tenth commit

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -8,7 +8,7 @@ function Contact() {
         <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
           <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
-  width="90%"
+  width="100%"
   height="100%"
   className="absolute inset-0"
   frameBorder="0"
