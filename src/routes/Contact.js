16f14a76eaf13ec464309d7ee5094335f1dce667
Yonaswg <110293537+Yonaswg@users.noreply.github.com>
2024-01-31 14:29:04 +0300

    seventh commit

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -50,7 +50,7 @@ function Contact() {
                 className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
-            <div className="relative mb-4">
+            <div className="relative ml-2 mr-2 mb-4">
               <label htmlFor="email" className="leading-7 text-sm text-gray-600">
                 Email
               </label>
@@ -61,7 +61,7 @@ function Contact() {
                 className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
-            <div className="relative mb-4">
+            <div className="relative ml-2 mr-2 mb-4">
               <label htmlFor="message" className="leading-7 text-sm text-gray-600">
                 Message
               </label>
@@ -71,7 +71,7 @@ function Contact() {
                 className="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
               ></textarea>
             </div>
-            <button className="text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+            <button className="text-white ml-10 mr-10 bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
               Submit
             </button>
             <p className="text-s text-gray-500 ml-9 mt-3">Empowering community through technology !</p>
