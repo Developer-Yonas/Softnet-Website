d360f4c30b64d38079ebf5f656497059d7de64ce
Yonaswg <110293537+Yonaswg@users.noreply.github.com>
2024-02-01 11:42:04 +0300

    tenth commit

--- src/routes/Service.js
+++ src/routes/Service.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import { Link  } from 'react-router-dom';
 import { IoMdSchool, IoIosArrowForward } from 'react-icons/io';
 import { FiServer } from 'react-icons/fi';
 import { AiOutlineCode } from 'react-icons/ai';
 import { FaRegBuilding } from 'react-icons/fa';
 
+
+
+
 const Service = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <div className="max-w-7xl mx-auto p-6 border rounded shadow-md">
+    <div id="top" className="max-w-7xl mx-auto p-6 border rounded shadow-md">
+    
       <h2 className="text-4xl text-violet-900 font-semibold mb-10"> Services</h2>
       <div className="flex flex-wrap -mx-5 cursor-pointer">
         <div className="w-full md:w-1/2 lg:w-1/2 px-4 mb-4">
-          <Link to="/services/TechnologyTraining">
+        <Link to="/services/TechnologyTraining" onClick={scrollToTop}>
             <div className="bg-white p-6 rounded border hover:bg-gray-100 transition duration-300">
               <div className="flex items-center justify-between">
                 <h3 className="text-2xl text-violet-800 font-semibold mb-2 flex items-center">
@@ -28,7 +37,7 @@ const Service = () => {
         </div>
 
         <div className="w-screen md:w-1/2 lg:w-1/2 px-4 mb-4">
-          <Link to="/services/Infrastructure">
+          <Link to="/services/Infrastructure" onClick={scrollToTop}>
             <div className="bg-white p-6 rounded border hover:bg-gray-50 transition duration-300">
               <div className="flex items-center justify-between">
                 <h3 className="text-2xl text-violet-800 font-semibold mb-2 flex items-center">
@@ -43,7 +52,7 @@ const Service = () => {
         </div>
 
         <div className="w-full md:w-1/2 lg:w-1/2 px-4 mb-4">
-          <Link to="/services/Softwaredevelopment">
+          <Link to="/services/Softwaredevelopment" onClick={scrollToTop}>
             <div className="bg-white p-6 rounded border hover:bg-gray-300 transition duration-300">
               <div className="flex items-center justify-between">
                 <h3 className="text-2xl text-violet-800 font-semibold mb-2 flex items-center">
@@ -59,7 +68,7 @@ const Service = () => {
         </div>
 
         <div className="w-full md:w-1/2 lg:w-1/2 px-4 mb-4">
-          <Link to="/services/ITO">
+          <Link to="/services/ITO" onClick={scrollToTop}>
             <div className="bg-white p-6 rounded border hover:bg-gray-300 transition duration-300">
               <div className="flex items-center justify-between">
                 <h3 className="text-2xl text-violet-800 font-semibold mb-2 flex items-center">
@@ -71,11 +80,11 @@ const Service = () => {
               <p className="text-gray-700 mt-5 text-lg hover:text-blue-500">Softnet, your ultimate partner for outsourced services that redefine excellence. Dive into a realm where professionalism</p>
             </div>
           </Link>
-        </div>
-        {/* Add more service items as needed */}
+        </div> 
+        {/* {/* Add more service items as needed */}
       </div>
     </div>
   );
 }
 
-export default Service;
+export default Service; 
